c5084fbfa6165136ddcf6f9e73dee88cf3bc8581
SteafanosPapanikolaou <papanikst@windowslive.com>
2026-01-09 23:47:32 +0200

    Created Client_Chaining_Agent.py, will use the Chaining_Agent as a sub-agent

---
+++ Backend/01_Chaining_Agent/Client_Chaining_Agent.py
@@ -0,0 +1,51 @@
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain.agents import create_agent
+from Backend.Connectors.LLM_Connector import LLMConnector
+import mcp
+import asyncio
+
+async def elicitation_callbak(context, params):
+    q = input(f"Elicitation requested: {params}")
+    return mcp.types.ElicitResult(action='accept', content={'value': q})
+
+class MCPClient:
+    def __init__(self):
+        self.llm = LLMConnector.llm_connect(model='qwen3:1.7b')
+        self.client = MultiServerMCPClient({
+            "main_server": {
+                "url": "http://localhost:8000/mcp",
+                "transport": "streamable_http",
+                "session_kwargs": {
+                    "elicitation_callback": elicitation_callbak,
+                }
+            }
+        })
+        self.tools = None
+        self.prompt = 'You are a helpful assistant.'
+        self.agent = None
+
+    async def setup(self):
+        self.tools = await self.client.get_tools()
+
+        self.agent = create_agent(model=self.llm, tools=self.tools, system_prompt=self.prompt)
+
+    async def generate_answer(self, query):
+        agent_answer = await self.agent.ainvoke({
+            "messages": [{"role": "user", "content": query}],
+        })
+
+        return agent_answer['messages'][-1].content
+
+
+async def main():
+    client = MCPClient()
+    await client.setup()
+    query = (
+        "Save to knowledge graph, my strategy. "
+        "Buy a house at 40% market value and sell it when people are buying stocks."
+    )
+    a = await client.generate_answer(query)
+    print(a)
+
+if __name__ == '__main__':
+    asyncio.run(main())
